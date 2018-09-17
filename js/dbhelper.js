99d221df6a76dfeecd94dcd4e2971d98fafd31c0
Sanmitra <goodfriend2899@gmail.com>
2018-09-17 23:05:06 +0530

    feat: now application pull restaurant data from development server

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,8 +8,13 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
+/*
     const port = 8000 // Change this to your server port
     return `http://localhost:${port}/data/restaurants.json`;
+*/
+  const port = 1337
+  return `http://localhost:${port}/restaurants`;
+
   }
 
   /**
@@ -21,7 +26,10 @@ class DBHelper {
     xhr.onload = () => {
       if (xhr.status === 200) { // Got a success response from server!
         const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
+        //const restaurants = json.restaurants;
+        const restaurants = json;
+        console.log("JSON = ", json);
+        console.log("REstaurants =  ", restaurants);
         callback(null, restaurants);
       } else { // Oops!. Got an error from server.
         const error = (`Request failed. Returned status of ${xhr.status}`);
