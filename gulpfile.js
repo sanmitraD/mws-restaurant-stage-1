666bcaf61d4354eba61ed89ed7c4c36509a95f7e
Sanmitra <goodfriend2899@gmail.com>
2018-09-26 00:13:20 +0530

    feat: css and js minification done

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const pngquant = require('imagemin-pngquant');
+const babel = require('gulp-babel');
+
+gulp.task("default",['copy-html','minify-js','styles','copy-sw','copy-manifest'], function() {
+  gulp.watch('sass/**/*.scss',['styles']);
+  gulp.watch('/*.html',['copy-html']);
+  gulp.watch('/sw.js',['copy-sw']);
+  //as manifest wouldn't be changed often so i didn't make it to watch
+});
+
+gulp.task('dist',['copy-html','styles','minify-js','copy-images','copy-sw','copy-manifest'])
+
+
+
+gulp.task('minify-js', function() {
+    return gulp.src(['js/dbhelper.js','js/main.js','js/restaurant_info.js'])
+    .pipe(babel({presets:['es2015']}))
+    .pipe(uglify())
+    .on("error",function(e) {
+      console.log(e);
+    })
+    .pipe(gulp.dest("dist/dbhelper.js"));
+});
+
+gulp.task('optimise-images', function() {
+    return gulp.src('img/*')
+        .pipe(imagemin({
+            progressive: true,
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task("styles", function() {
+    gulp
+    .src("sass/**/*.scss")
+    .pipe(sass({outputStyle: 'compressed'}))
+    .on("error", sass.logError)
+    .pipe(
+      autoprefixer({
+        browsers:['last 3 versions']
+      })
+    )
+    .pipe(gulp.dest("./dist/css"));
+});
+
+gulp.task('copy-html',function() {
+    gulp.src('./*.html')
+    .pipe(gulp.dest('./dist'));
+});
+gulp.task('copy-manifest',function() {
+    gulp.src('./manifest.json')
+    .pipe(gulp.dest('./dist'));
+});
+gulp.task('copy-sw',function() {
+    gulp.src('./sw.js')
+    .pipe(gulp.dest('./dist'));
+});
